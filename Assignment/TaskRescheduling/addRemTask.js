aa4cd8f5c788033875546dd5e87c165dd6a3240b
vishal12321git <vishalpandey7594@gmail.com>
2025-01-30 17:53:05 +0530

    addRemove taks

---
+++ Assignment/TaskRescheduling/addRemTask.js
@@ -0,0 +1,6 @@
+
+let toDo = ["Complete project report", "Reply to emails", "Team meeting", "Review code", "Plan next sprint"]
+
+toDo.unshift("Fix urgent bug","prepare documentation")
+toDo.pop()
+console.log(toDo)
