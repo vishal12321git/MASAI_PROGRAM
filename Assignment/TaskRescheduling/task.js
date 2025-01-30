93a2ed44acb5540fbed810f9256d8f4c2e48b960
vishal12321git <vishalpandey7594@gmail.com>
2025-01-30 13:52:50 +0530

    third

---
+++ Assignment/TaskRescheduling/task.js
@@ -0,0 +1,8 @@
+let tasks = ["Complete project report", "Reply to emails", "Team meeting", "Review code", "Plan next sprint"];
+
+tasks[0]="Fix urgent bug"
+tasks[1]="Update documentation"
+
+tasks[tasks.length - 1] = "Update documentation";
+
+console.log(tasks);
