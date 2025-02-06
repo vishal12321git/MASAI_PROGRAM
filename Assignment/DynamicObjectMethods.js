8e07266f853e5f5e60a907118bd2999b81e0a6cc
vishal12321git <vishalpandey7594@gmail.com>
2025-02-06 19:23:31 +0530

    dynamic object methods

---
+++ Assignment/DynamicObjectMethods.js
@@ -0,0 +1,29 @@
+let sentenceBuilder = {
+    subject:"I",
+    verb:"am",
+    object:"coding",
+    buildSentence:function(){
+       return (this.subject && this.verb && this.object)?
+              `${this.subject} ${this.verb} ${this.object}`:
+              "Incomplete sentence"
+    },
+    updateProperty:function(property,value){
+        return (this.hasOwnProperty(property))?
+              ( this[property]=value,"Property Updated"):
+              "Invalid property";
+    }
+}
+
+console.log(sentenceBuilder.buildSentence());
+
+sentenceBuilder.updateProperty("verb", "am learning");
+console.log(sentenceBuilder.buildSentence()); 
+
+sentenceBuilder.updateProperty("subject", "The cat");
+console.log(sentenceBuilder.buildSentence());
+
+sentenceBuilder.updateProperty("mood", "happy");
+console.log(sentenceBuilder.updateProperty("mood", "happy")); 
+
+sentenceBuilder.updateProperty("verb", "");
+console.log(sentenceBuilder.buildSentence()); 
